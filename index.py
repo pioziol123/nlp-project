1ad65744c6ad241fffc2ef97b242a3739d6b6e11
Piotr Ziółkowski <pioziol@gmail.com>
2019-06-18 21:57:34 +0200

    regex classifier introduced

--- index.py
+++ index.py
@@ -2,6 +2,7 @@ from sklearn.model_selection import train_test_split
 from m_l_fitter_factory import MLFitterFactory
 from sklearn.datasets import fetch_20newsgroups
 from model_evaluator import ModelEvaluator
+from regex_classifier import RegexClassifier
 
 fitter = MLFitterFactory().create({'model': 'sgd'})
 train_data = fetch_20newsgroups(subset='all', shuffle=True)
@@ -24,3 +25,9 @@ fitter.model_preceptor.use_xgb_model()
 fitter.train(x_train, y_train)
 predictions = fitter.predict(x_test)
 print(ModelEvaluator.accuracy(predictions, y_test))
+
+print('\n\n---RegexClassifier---\n')
+classifier = RegexClassifier()
+classifier.train(x_train, y_train)
+predictions = classifier.predict(x_test)
+print(ModelEvaluator().accuracy(predictions, y_test))
